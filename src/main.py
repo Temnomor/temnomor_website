24f0f58672631e34a10765682c33f1aaa7c3606f
ConfirmedPlayer <confirmedplayer@mail.ru>
2024-02-12 02:08:08 +0500

    rollback to uvicorn

--- src/main.py
+++ src/main.py
@@ -2,14 +2,16 @@ import asyncio
 import os
 import shutil
 from argparse import ArgumentParser
+from typing import NoReturn
 
+import uvicorn
 from apscheduler.schedulers.asyncio import AsyncIOScheduler, BaseScheduler
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from loguru import logger
 
-from gunicorn.app.base import BaseApplication
-from constants import LOGGER_FORMAT_TELEGRAM_BOT, OPTIONS
+from constants import (HOST_PORT, LOGGER_FORMAT_TELEGRAM_BOT,
+                       SSL_CERTFILE_PATH, SSL_KEYFILE_PATH)
 from exceptions import exception_handlers_dict
 from routers import api_routers
 from utils.logging_handler import log_to_telegram_bot
@@ -31,23 +33,6 @@ app.mount('/static', StaticFiles(directory='static'), name='static')
 app.exception_handlers = exception_handlers_dict
 
 
-class StandaloneApplication(BaseApplication):
-
-    def __init__(self, app, options=None):
-        self.options = options or {}
-        self.application = app
-        super().__init__()
-
-    def load_config(self):
-        config = {key: value for key, value in self.options.items()
-                  if key in self.cfg.settings and value is not None}
-        for key, value in config.items():
-            self.cfg.set(key.lower(), value)
-
-    def load(self):
-        return self.application
-
-
 @logger.catch
 async def clean_up(scheduler: BaseScheduler = None):
     if scheduler and scheduler.running:
@@ -57,6 +42,23 @@ async def clean_up(scheduler: BaseScheduler = None):
     if os.path.exists('temp'):
         shutil.rmtree('temp', ignore_errors=True)
         logger.info('Folder "temp" was deleted')
+
+
+@logger.catch
+async def run_server() -> NoReturn:
+    config = uvicorn.Config(
+        'main:app',
+        port=HOST_PORT,
+        log_level='info',
+        workers=9,
+        forwarded_allow_ips='*',
+        ssl_keyfile=SSL_KEYFILE_PATH,
+        ssl_certfile=SSL_CERTFILE_PATH
+        )
+
+    server = uvicorn.Server(config)
+
+    await server.serve()
 
 
 @logger.catch
@@ -87,8 +89,7 @@ async def main() -> None:
         scheduler.start()
         logger.info('Scheduler started. Running server...')
 
-        StandaloneApplication(app=app, options=OPTIONS).run()
-
+        await run_server()
     finally:
         await clean_up(scheduler=scheduler)
 
