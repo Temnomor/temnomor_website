2024555458053d8b72978241b1ddbfb55f0f260f
ConfirmedPlayer <confirmedplayer@mail.ru>
2024-02-12 16:03:15 +0500

    changed headers

--- src/constants.py
+++ src/constants.py
@@ -20,13 +20,13 @@ SCHEDULE_FORMS_URLS = (
 
 
 HEADERS = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Upgrade-Insecure-Requests': '1',
-    'Host': 'sh.mnokol.tyuiu.ru',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.2 Safari/605.1.15',
-    'Referer': 'http://sh.mnokol.tyuiu.ru/shold/index2.php',
-    'Accept-Language': 'en-US,en;q=0.9',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'Accept-Language': 'en-US,en;q=0.9,ru;q=0.8,tg;q=0.7',
+    'Cache-Control': 'no-cache',
     'Connection': 'keep-alive',
+    'Pragma': 'no-cache',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
 }
 
 
