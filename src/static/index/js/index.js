2afbc12e9c89ae5d88904f26672a6b3849893371
Confi <48089790+ConfirmedPlayer@users.noreply.github.com>
2024-02-15 02:09:21 +0500

    #2 clipboard fix

--- src/static/index/js/index.js
+++ src/static/index/js/index.js
@@ -265,7 +265,6 @@ function copyToClipboard(elem)
   searchInp4.select();
   searchInp4.setSelectionRange(0, 99999);
   navigator.clipboard.writeText(lecturer);
-  alert(`ФИО преподавателя (${lecturer}) скопировано в буфер обмена`);
 }
 
 async function loadLecturersFullName()
