7f283fd68b53ae3aae0e4a9835fff58e5e132c62
Confi <confirmedplayer@mail.ru>
2024-08-27 13:11:20 +0500

    updated user agent

--- src/constants.py
+++ src/constants.py
@@ -23,16 +23,15 @@ SCHEDULE_GROUP_HEADERS = {
     'cache-control': 'no-cache',
     'pragma': 'no-cache',
     'priority': 'u=0, i',
-    'referer': '!!!needs_to_be_unique_for_each_group_all_the_time!!!',
-    'sec-ch-ua': '"Chromium";v="124", "Microsoft Edge";v="124", "Not-A.Brand";v="99"',
+    'sec-ch-ua': '"Chromium";v="128", "Not;A=Brand";v="24", "Microsoft Edge";v="128"',
     'sec-ch-ua-mobile': '?0',
     'sec-ch-ua-platform': '"Windows"',
-    'sec-fetch-dest': 'iframe',
+    'sec-fetch-dest': 'document',
     'sec-fetch-mode': 'navigate',
     'sec-fetch-site': 'same-origin',
     'sec-fetch-user': '?1',
     'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0',
 }
 
 SCHEDULE_FORM_HEADERS = {
@@ -42,7 +41,7 @@ SCHEDULE_FORM_HEADERS = {
     'pragma': 'no-cache',
     'priority': 'u=0, i',
     'referer': 'https://coworking.tyuiu.ru/shs/shedule.php',
-    'sec-ch-ua': '"Chromium";v="124", "Microsoft Edge";v="124", "Not-A.Brand";v="99"',
+    'sec-ch-ua': '"Chromium";v="128", "Not;A=Brand";v="24", "Microsoft Edge";v="128"',
     'sec-ch-ua-mobile': '?0',
     'sec-ch-ua-platform': '"Windows"',
     'sec-fetch-dest': 'iframe',
@@ -50,7 +49,7 @@ SCHEDULE_FORM_HEADERS = {
     'sec-fetch-site': 'same-origin',
     'sec-fetch-user': '?1',
     'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0',
 }
 
 SCHEDULE_LECTURERS_HEADERS = {
